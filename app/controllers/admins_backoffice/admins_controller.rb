057328af08aa354a45aa51c8c4f93f1eb125525e
MateusGomes04 <mateusgomesc04@gmail.com>
2023-01-25 08:10:57 -0300

    feat: adding New and Create to register new administrators

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -2,6 +2,19 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def index
     @admins = Admin.all
   end
+
+  def new
+     @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
   def edit
    @admin = Admin.find(params[:id])
   end
@@ -19,4 +32,16 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
       render :edit
   end
 end
+private
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+  def verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
 end
