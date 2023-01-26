e3a88cdb68af4d4cb50c71d5786495df26b4f1fe
MateusGomes04 <mateusgomesc04@gmail.com>
2023-01-26 10:00:34 -0300

    feat: Creating views (new and edit) for Subject_Areas

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -1,7 +1,7 @@
 class AdminsBackoffice::SubjectsController < AdminsBackofficeController
     before_action :set_subject, only: [:edit, :update, :destroy]
     def index
-      @subjects = Subject.all.page(params[:page])
+      @subjects = Subject.all.order(:description).page(params[:page])
     end
   
     def new
@@ -11,7 +11,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
     def create
       @subject = Subject.new(params_subject)
       if @subject.save
-        redirect_to admins_backoffice_subjects_path, notice: "Administrador cadastrado com sucesso!"
+        redirect_to admins_backoffice_subjects_path, notice: "Assunto/area cadastrado com sucesso!"
       else
         render :new
       end
@@ -19,20 +19,14 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
     def edit
      @subject = Subject.find(params[:id])
     end
+
     def update
-      if params[:subject][:password].blank? && params[:subject][:password_confirmation].blank?
-        params[:subject].extract!(:password, :password_confirmation)
-      end  
-  
-      @subject = Subject.find(params[:id])
-      params_subject = params.require(:subject).permit(:email, :password, :password_confirmation)
-  
       if @subject.update(params_subject)
-        redirect_to admins_backoffice_subjects_path, notice: "assunto/area atualizado com sucesso!"
+        redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área atualizado com sucesso!"
       else
         render :edit
-    end
-  end
+      end
+    end  
   
   def destroy
     if @subject.destroy
