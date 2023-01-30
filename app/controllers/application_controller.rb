8f8a5359d2459924dce200a4a0ea8bb6cf31cf98
MateusGomes04 <mateusgomesc04@gmail.com>
2023-01-30 12:24:36 -0300

    feat: Migrating the User Devise login template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,9 @@ class ApplicationController < ActionController::Base
     layout :layout_by_resource
 
     protected
-  
-      def layout_by_resource
-        if devise_controller? && resource_class == Admin
-          "admin_devise"
-        else
-          "application"
-        end
-      end
+
+    def layout_by_resource
+    devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
+
+    end
 end
